101264724bdaf39dc7789a84d8ac70c2b2a689c9
Santiago Varela <s.javiervarela@gmail.com>
2019-04-09 11:50:24 -0300

    Just testing

--- ReactNetApp/Program.cs
+++ ReactNetApp/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+//https://www.codingame.com/playgrounds/35462/creating-web-api-in-asp-net-core-2-0/part-1---web-api
+
 namespace ReactNetApp
 {
     public class Program
--- ReactNetApp/Startup.cs
+++ ReactNetApp/Startup.cs
@@ -27,6 +27,7 @@ namespace ReactNetApp
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
